08a6ae233491952f00a9e763ff258f99c71cf17e
karow <karow.foxx@gmail.com>
2021-07-31 18:15:40 +0100

    - notes activity has hardcoded list of items

--- app/src/main/java/com/minote/NotesActivity.kt
+++ app/src/main/java/com/minote/NotesActivity.kt
@@ -2,10 +2,19 @@ package com.minote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class NotesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notes)
+
+        val noteList: ListView = findViewById(R.id.notes)
+
+        val listItems = ArrayList<String>()
+        listItems.add("hello")
+        listItems.add("world")
+        noteList.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems)
     }
 }
